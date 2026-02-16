fb6c95c352bfebbf8c0b0501e9a911cc25c641fd
DevZakariaElkashef <dev.zakariaelkashef@gmail.com>
2026-02-16 20:19:27 +0200

    Add except show to companyTransportations resource route

--- routes/web.php
+++ routes/web.php
@@ -298,7 +298,7 @@ Route::group(['namespace' => 'App\Http\Controllers\Admin', 'middleware' => ['aut
     // ----------------- Transportation -----------------
     Route::get('companyTransportations/export', [CompanyTransportationController::class, 'export'])->name('companyTransportations.export');
     Route::post('companyTransportations/import', [CompanyTransportationController::class, 'import'])->name('companyTransportations.import');
-    Route::resource('companyTransportations', CompanyTransportationController::class);
+    Route::resource('companyTransportations', CompanyTransportationController::class)->except(['show']);
     Route::resource('{company}/companyServices', CompanyServicesController::class)->except('show');
     Route::post('{company}/companyServices/import', [CompanyServicesController::class, 'import'])->name('companyServices.import');
     // ----------------- \Transportation -----------------
