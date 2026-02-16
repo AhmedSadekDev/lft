583ea0c4e6eddf2166a7ffb464a44011d7003d9c
DevZakariaElkashef <dev.zakariaelkashef@gmail.com>
2026-02-16 22:39:19 +0200

    Fix KTAppSettings undefined error: add default definition before scripts.bundle.js

--- resources/views/layouts/includes/footer.blade.php
+++ resources/views/layouts/includes/footer.blade.php
@@ -1,5 +1,30 @@
 <!--end::Global Config-->
 <!--begin::Global Theme Bundle(used by all pages)-->
+<script>
+    // Define KTAppSettings if not already defined
+    if (typeof KTAppSettings === 'undefined') {
+        var KTAppSettings = {
+            breakpoints: { sm: 576, md: 768, lg: 992, xl: 1200, xxl: 1400 },
+            colors: {
+                'state': {
+                    'brand': '#5d78ff',
+                    'dark': '#282a3c',
+                    'light': '#ffffff',
+                    'primary': '#5867dd',
+                    'success': '#34bfa3',
+                    'info': '#36a3f7',
+                    'warning': '#ffb822',
+                    'danger': '#fd27eb'
+                },
+                'base': {
+                    'label': ['#c5cbe3', '#a1a8c3', '#3d4465', '#3e4466'],
+                    'shape': ['#f0f3ff', '#d9dffa', '#afb4d4', '#646c9a']
+                }
+            },
+            fontFamily: 'Poppins'
+        };
+    }
+</script>
 <script src="{{ asset('assets/plugins/global/plugins.bundle.js') }}"></script>
 <script src="{{ asset('assets/js/scripts.bundle.js') }}"></script>
 <!--end::Global Theme Bundle-->
