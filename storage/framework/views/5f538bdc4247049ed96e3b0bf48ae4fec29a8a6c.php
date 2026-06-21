887ddf2f7b73b3cbbd3a587a606e4fdfa38393e1
DevZakariaElkashef <dev.zakariaelkashef@gmail.com>
2026-06-21 23:17:23 +0300

    Remove obsolete view files related to invoice creation, company management, employee editing, and various components to streamline the codebase and improve maintainability.

--- storage/framework/views/5f538bdc4247049ed96e3b0bf48ae4fec29a8a6c.php
+++
@@ -1,7 +0,0 @@
-
-<?php $__env->startSection('content'); ?>
-    <?php echo $__env->make('admin.components.booking-invoices.listings', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
-    <?php echo $__env->make('admin.components.booking-invoices.totals-form', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
-<?php $__env->stopSection(); ?>
-
-<?php echo $__env->make('layouts.admin', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH /home3/cloudtal/cloudymenue.cloudy-digital.com/resources/views/admin/bookings/booking-invoices/edit.blade.php ENDPATH**/ ?>
--- storage/framework/views/c02c88a5fcc6692f2a2ff8240980dced5d74244b.php
+++
@@ -1,2 +0,0 @@
-
-<?php /**PATH /home3/cloudtal/cloudymenue.cloudy-digital.com/resources/views/admin/bookings/thirdStep/clearance.blade.php ENDPATH**/ ?>
